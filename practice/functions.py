15fc8fe8b11ce718b8931f6f246bb522154b1337
thulasibatchu <bthulasi2552002@gmail.com>
2025-06-17 23:59:56 +0530

    func

---
+++ practice/functions.py
@@ -0,0 +1,49 @@
+# def greet():
+#     print("hey Thulasi")
+
+# greet()
+
+# def add(a,b):
+#     print(a+b)
+
+# add(1,2)
+
+# def number(a):
+#     if a%2==0:
+#         print('even')
+#     else:
+#         print('odd')
+
+# number(7)
+
+# def factorial(a):
+#     prod=1
+#     for i in range(1,a+1):
+#       prod=prod*i
+#     print(prod)
+
+# factorial(3)
+
+# nums=[1,2,3]
+# sum=0
+# for num in nums:
+#     sum=sum+num
+# print(sum)
+
+fruits = ["apple", "banana", "mango","apple","mango"]
+print(fruits.count('apple'))
+
+#num=[2,5,7,4,98]
+#print(max(num))
+
+# nums=[2,5,7,4,98,4,90,34,23,75]
+# odd=[]
+# for num in nums:
+#     if num%2!=0:
+#         odd.append(num)
+# print(odd)
+
+
+
+
+      
