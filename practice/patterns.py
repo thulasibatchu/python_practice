5a365021d992437fd75f645ff29b5b38095ce727
thulasibatchu <bthulasi2552002@gmail.com>
2025-07-18 16:31:48 +0530

    patterns

--- practice/patterns.py
+++ practice/patterns.py
@@ -1,8 +1,64 @@
 # for i in range(1,6):
 #     print("*"*i)
 
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
-    
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#   4,1,4 3,3,3 2,5,2 1,7,1 0,9,0
+
+# palindrome pattern
+# n=5
+# for i in range(1,n+1):
+#      for j in range(n-i):
+#           print(" ",end="")
+#      for j in range(1,i+1):
+#           print(j,end="")
+     
+#      for j in range(i-1,0,-1):
+#           print(j,end="")
+#      print()
+
+# increasing number pyramid
+
+# n=4
+# count=1
+# for i in range(1,n+1):
+#     for j in range(1,n-i+1):
+#         print(" ",end="")
+#     for j in range(1,i+1):
+#         print(count,end=" ")
+#         count+=1
+#     print()
+
+# number pyramid
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+#inverted number pyramid
+# n=5
+# for i in range(n,0,-1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+#diamond shape
+n=5
+for i in range(n+1):
+    for j in range():
+        pass
+
+
+
+
+
+
+
+
